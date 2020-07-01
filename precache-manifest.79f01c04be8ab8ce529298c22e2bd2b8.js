0ecebdb8602b12723d4189023264ffc3ccdcd652
nimishbongale <nimishnb98@gmail.com>
2020-07-01 20:32:15 +0000

    Deploying to gh-pages from  @ 1723d0e9b4ad06a9dd0731f172d98e9b366e5332 🚀

---
+++ precache-manifest.79f01c04be8ab8ce529298c22e2bd2b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86e784f581f2db0a24a9fc435694ee1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f5e9a62ee6afff29bed",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de1aebe8fd88ad47ab56",
+    "url": "/static/js/2.64351eae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64351eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5e9a62ee6afff29bed",
+    "url": "/static/js/main.d41b1b7d.chunk.js"
+  },
+  {
+    "revision": "cb2b141ee43e801e8c5c",
+    "url": "/static/js/runtime-main.14ce0136.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
