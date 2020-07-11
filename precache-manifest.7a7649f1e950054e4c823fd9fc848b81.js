ce2e57f23952a55384465102ccd2a0ebdafff777
nimishbongale <nimishnb98@gmail.com>
2020-07-11 04:43:33 +0000

    Deploying to gh-pages from  @ b1e099a752c47b65c15c4608d29501ebfd45bd87 🚀

---
+++ precache-manifest.7a7649f1e950054e4c823fd9fc848b81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3369736f80f159f71b9471558b49d34e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63524765284cf7114ebe",
+    "url": "./static/css/main.6993f0a6.chunk.css"
+  },
+  {
+    "revision": "79c282c4d02820bbd8db",
+    "url": "./static/js/2.08ae187c.chunk.js"
+  },
+  {
+    "revision": "2364e507b571f95c4bd8e70983dc8a6c",
+    "url": "./static/js/2.08ae187c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63524765284cf7114ebe",
+    "url": "./static/js/main.e865c7aa.chunk.js"
+  },
+  {
+    "revision": "2306f4582c4679a946aa",
+    "url": "./static/js/runtime-main.53aa38d8.js"
+  },
+  {
+    "revision": "f99b03c55c87f03f4a8528606a0861eb",
+    "url": "./static/media/aadhar.f99b03c5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35c408e5eb584543efdc9d5c5551d160.js"
+  "./precache-manifest.7a7649f1e950054e4c823fd9fc848b81.js"
 );
 
 self.addEventListener('message', (event) => {
