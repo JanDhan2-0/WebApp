209cb46e96ebe9428842ec25477b10a34db43c6d
Tanisha <tanisha031199@gmail.com>
2020-07-21 19:11:29 +0530

    fixed search form

--- src/pages/Records.js
+++ src/pages/Records.js
@@ -5,43 +5,8 @@ import MySearchBar from '../components/basic/SearchBar';
 import Tabular from "../components/basic/Tabular"
 
 export default function Test() {
-  let data=[
-      {
-          id:"SBI123",
-          date:"05/06/20",
-          name:"Raj Malhotra",
-          phone:"9645235976",
-          purpose:"A/C creation",
-          status:"Pending"
-      },
-      {
-        id:"SBI563",
-        date:"21/06/20",
-        name:"Sooraj Kapoor",
-        phone:"9645235976",
-        purpose:"A/C creation",
-        status:"Approved"
-    },
-    {
-        id:"SBI223",
-        date:"05/07/20",
-        name:"Raj Malhotra",
-        phone:"9645235976",
-        purpose:"A/C creation",
-        status:"Rejected"
-    },
-    {
-        id:"SBI423",
-        date:"05/06/20",
-        name:"Raj Malhotra",
-        phone:"9645235976",
-        purpose:"A/C creation",
-        status:"Pending"
-    },
-    ];
     const headers=["Form Id","Date","Name","Ph no","Purpose","Status"]
     const [value, setValue] = useState("");
-    const [rec, setRec] = useState(data);
     const [records, setRecords] = useState([]);
     useEffect(()=>{
       axios.get('http://jandhan2.herokuapp.com/account/bank/SBI/getAll ')
@@ -52,7 +17,7 @@ export default function Test() {
     },[]);
     function handleChange(newValue) {
       setValue(newValue);
-      setRec(data.filter(item => item.id.includes(newValue)))
+      setRecords(records.filter(item => item.formId.includes(newValue)))
     }
 
   return (
