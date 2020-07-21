1220f3d121601dd74d57a59e338d678d352dc71c
Tanisha <tanisha031199@gmail.com>
2020-07-21 17:55:38 +0530

    integerated to feedback API

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,56 +1,61 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
 import Rating from '@material-ui/lab/Rating';
 import MyAppBar from '../components/basic/AppBar'
 import MyProgress from '../components/basic/Progress'
 
 export default function MyRecord() {
+  const [reviews, setReviews]=useState(null);
   const style={
     display:'flex',
     padding:'20px'
   };
-  const data=[
-    {
-      name:"Rajendra Shah",
-      date:"11th July",
-      review:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
-    },
-    {
-      name:"Chhavi Malhotra",
-      date:"12th July",
-      review:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
-    },
-    {
-      name:"Anushka Singh",
-      date:"15th July",
-      review:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
-    },
-  ];
-  const reviews = [60,40,30,10,5]
+  const formatDate = (date) => {
+    if(date===null) return;
+    var dob = new Date(date);
+    var dobArr = dob.toDateString().split(' ');
+    var dobFormat = dobArr[2] + ' ' + dobArr[1] + ' ' + dobArr[3];
+    return dobFormat;
+  };
+  useEffect(()=>{
+    axios.get('http://jandhan2.herokuapp.com/feedback/bank/SBI/touchPoint/ATM/reviews')
+    .then(res => {
+      console.log(res.data)
+      setReviews(res.data);
+    })
+    .catch(err=>console.log(err));
+  },[]);
   return (
     <div style={{flexGrow:1}}>
       <MyAppBar active="feedback"/><br></br>
+      {
+        reviews===null ? <div>Loading</div> :(
+          <div>
       <div style={style}>
         <div style={{width:"30%", padding:"0 20px"}}>
-          <h1>4.2</h1>
-          <Rating name="read-only" value={4.2} readOnly />
-          <div>Based on 21 Reviews</div><br></br>
-           {reviews.map((item,index)=>
+        <h1>{reviews.averageRating.toFixed(2)}</h1>
+          <Rating name="read-only" value={reviews.averageRating.toFixed(2)} precision={0.25} readOnly />
+          <div>Based on {reviews.totalReviews} Reviews</div><br></br>
+           {Object.values(reviews.ratingDetails).reverse().map((item,index)=>
              <MyProgress value={item} rating={5-index} key={index}/>
            )}
         </div>
         <div style={{width:"70%", padding:"20px 0 0"}}>
-          <h3>Reviews (21)</h3>
+          <h3>Reviews ({reviews.totalReviews})</h3>
             {
-              data.map(item =>
+              reviews.response.map(item =>
                 <div style={  {backgroundColor:"#EAEAEA", padding:"10px 20px", marginBottom:"20px", borderRadius:"5px"}}>
-                  <h3>{item.name}</h3>
-                  <p>{item.date}</p>
-                  <p>{item.review}</p>
+                  <h3 style={{marginBottom:"2px"}}>{item.feedbackBy}</h3>
+                  <p style={{margin:0, fontSize:"12px"}}>{formatDate(item.date)}</p>
+                  <p>{item.feebdack}</p>
                 </div>
               )
             }
         </div>
       </div>
+      </div>
+      )
+      }
     </div>
   );
 }
